1e7d3eb1d2ec95e25d147fda11b8cc30eaf1c1ac
Raushan Raj <kumarraushanraj10@gmail.com>
2025-07-31 02:06:06 +0530

    error in deploy

--- Aarambh38_Frontend/vite.config.js
+++ Aarambh38_Frontend/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/', // 👈 this is essential for correct asset loading
   plugins: [react()],
 })
