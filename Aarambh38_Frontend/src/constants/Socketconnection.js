c597c784fb113201720b8063ac5c1adafda0209f
Raushan Raj <kumarraushanraj10@gmail.com>
2025-08-21 19:51:14 +0530

    all set2

--- Aarambh38_Frontend/src/constants/Socketconnection.js
+++ Aarambh38_Frontend/src/constants/Socketconnection.js
@@ -1,17 +1,17 @@
-// import {io} from "socket.io-client"
-// import { BASE_URL } from "./AllUrl"
-
-// export const SocketConnection=()=>{
-//     if(location.hostname==="localhost")
-//         return io(BASE_URL)
-//     else
-//         return io("http://13.60.166.165",{path:"/api/socket.io"})
-// }
-
-
 import {io} from "socket.io-client"
 import { BASE_URL } from "./AllUrl"
 
 export const SocketConnection=()=>{
-    return io(BASE_URL)
+    if(location.hostname==="localhost")
+        return io(BASE_URL)
+    else
+        return io("http://13.60.166.165",{path:"/api/socket.io"})
 }
+
+
+// import {io} from "socket.io-client"
+// import { BASE_URL } from "./AllUrl"
+
+// export const SocketConnection=()=>{
+//     return io(BASE_URL)
+// }
