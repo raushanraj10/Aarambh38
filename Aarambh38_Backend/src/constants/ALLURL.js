822f097be4ff83f47568105a397041ed57b403d8
Raushan Raj <kumarraushanraj10@gmail.com>
2026-01-25 16:22:05 +0530

    filed set

--- Aarambh38_Backend/src/constants/ALLURL.js
+++ Aarambh38_Backend/src/constants/ALLURL.js
@@ -1,5 +1,5 @@
-const BASE_URL="http://localhost:5173"
-// const BASE_URL="https://aarambh38.vercel.app"
+// const BASE_URL="http://localhost:5173"
+const BASE_URL="https://aarambh38.vercel.app"
 
 
 module.exports=BASE_URL
